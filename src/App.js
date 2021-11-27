c0f5e04b3da54c2f68cc85435642c24218d6abf2
bonot9434 <bonot9434@gmail.com>
2021-11-27 16:34:19 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const App = props => {
   const [state,setState] = useState(props)
   const {name, price} = state
 
+  // useEffectはrenderの後で実行される
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate')
+  })
+  
+  // 空の第二引数は初回のみuseEffect
+  useEffect(() => {
+    console.log('This is like componentDidMount')
+  }, [])
+
+  // 特定、変更時のみ
+  useEffect(() => {
+    console.log('This callback is for name only')
+  }, [name])
+
   return (
     <>
-      <p>現在の{name}は、{price}円です</p>
+      <p>現在の{name}は、{price}円です。</p>
       <button onClick={() => setState({...state, price: price+1})}>+1</button>
       <button onClick={() => setState({...state, price: price-1})}>-1</button>
       <button onClick={() => setState(props)}>Reset</button>
