e0f8c085adb1f10abeea8e13b938901e0d82fe36
bonot9434 <bonot9434@gmail.com>
2021-11-27 17:26:36 +0900

    bootstrap追加

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 
-const App = props => {
-  const [state,setState] = useState(props)
-  const {name, price} = state
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-  // useEffectはrenderの後で実行される
-  useEffect(() => {
-    console.log('This is like componentDidMount or componentDidUpdate')
-  })
-  
-  // 空の第二引数は初回のみuseEffect
-  useEffect(() => {
-    console.log('This is like componentDidMount')
-  }, [])
-
-  // 特定、変更時のみ
-  useEffect(() => {
-    console.log('This callback is for name only')
-  }, [name])
+const App = () => {
 
   return (
-    <>
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setState({...state, price: price+1})}>+1</button>
-      <button onClick={() => setState({...state, price: price-1})}>-1</button>
-      <button onClick={() => setState(props)}>Reset</button>
-      <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
-    </>
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor ="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor ="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody" />
+        </div>
+
+        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-danger">全てのイベントを削除する</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
   )
 }
-
-App.defaultProps = {
-  name: '',
-  price: 1000
-}
-
 
 export default App;
