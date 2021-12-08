21c6d4e596acfc5a01bdd5fdd1a3319b1854cdd1
bonot9434 <bonot9434@gmail.com>
2021-12-08 22:34:49 +0900

    reduxインストール

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import React, {useReducer} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
-
 import Events from './Events'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
